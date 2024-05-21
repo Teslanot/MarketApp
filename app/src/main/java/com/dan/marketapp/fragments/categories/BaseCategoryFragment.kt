267f4a8fa7d79fcdc3b9956d181112de63063a64
Teslanot <daniilaleks8@gmail.com>
2024-05-21 20:24:31 +0300

    1.36(all categories view)

--- app/src/main/java/com/dan/marketapp/fragments/categories/BaseCategoryFragment.kt
+++ app/src/main/java/com/dan/marketapp/fragments/categories/BaseCategoryFragment.kt
@@ -4,17 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.dan.marketapp.R
 import com.dan.marketapp.adapters.BestProductAdapter
 import com.dan.marketapp.databinding.FragmentBaseCategoryBinding
 
 open class BaseCategoryFragment: Fragment(R.layout.fragment_base_category) {
     private lateinit var binding: FragmentBaseCategoryBinding
-    private lateinit var offerAdapter: BestProductAdapter
-    private lateinit var bestProductsAdapter: BestProductAdapter
+    protected val offerAdapter: BestProductAdapter by lazy { BestProductAdapter() }
+    protected val bestProductsAdapter: BestProductAdapter by lazy { BestProductAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,10 +32,48 @@ open class BaseCategoryFragment: Fragment(R.layout.fragment_base_category) {
 
         setupOfferRv()
         setupBestProductsRv()
+
+        binding.rvOffer.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                if(!recyclerView.canScrollVertically(1) && dx !=0){
+                    onOfferPagingRequest()
+                }
+            }
+        })
+
+        binding.nestedScrollBaseView.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener { v, _, scrollY, _, _ ->
+            if (v.getChildAt(0).bottom <= v.height + scrollY) {
+                onBestProductsPagingRequest()
+            }
+        })
+    }
+
+    fun showOfferLoading(){
+        binding.offerProductsProgressBar.visibility = View.VISIBLE
+    }
+    fun hideOfferLoading(){
+        binding.offerProductsProgressBar.visibility = View.GONE
+    }
+    fun showBestProductsLoading(){
+        binding.bestProductsProgressBar.visibility = View.VISIBLE
+    }
+    fun hideBestProductsLoading(){
+        binding.bestProductsProgressBar.visibility = View.GONE
+    }
+
+
+
+    open fun onOfferPagingRequest(){
+
+    }
+
+    open fun onBestProductsPagingRequest(){
+
     }
 
     private fun setupBestProductsRv() {
-        bestProductsAdapter = BestProductAdapter()
         binding.rvBestProducts.apply {
             layoutManager =
                 GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
@@ -42,8 +82,7 @@ open class BaseCategoryFragment: Fragment(R.layout.fragment_base_category) {
     }
 
     private fun setupOfferRv() {
-        offerAdapter = BestProductAdapter()
-        binding.rvBestProducts.apply {
+        binding.rvOffer.apply {
             layoutManager =
                 LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false )
             adapter = offerAdapter
--- app/src/main/java/com/dan/marketapp/fragments/categories/CupboardFragment.kt
+++ app/src/main/java/com/dan/marketapp/fragments/categories/CupboardFragment.kt
@@ -1,4 +1,66 @@
 package com.dan.marketapp.fragments.categories
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.dan.marketapp.data.Category
+import com.dan.marketapp.util.Resource
+import com.dan.marketapp.viewmodel.CategoryViewModel
+import com.dan.marketapp.viewmodel.factory.BaseCategoryViewModelFactory
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import javax.inject.Inject
+
+@AndroidEntryPoint
 class CupboardFragment:BaseCategoryFragment() {
+
+
+    @Inject
+    lateinit var firestore: FirebaseFirestore
+
+    val viewModel by viewModels<CategoryViewModel>{
+        BaseCategoryViewModelFactory(firestore, Category.Cupboard)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        lifecycleScope.launchWhenStarted {
+            viewModel.offerProducts.collectLatest{
+                when (it){
+                    is Resource.Loading ->{
+                        showOfferLoading()
+                    }
+                    is Resource.Success ->{
+                        offerAdapter.differ.submitList(it.data)
+                        hideOfferLoading()
+                    }
+                    is Resource.Error ->{
+                        Toast.makeText(requireContext(), it.message.toString(), Toast.LENGTH_SHORT).show()
+                        hideOfferLoading()
+                    }
+                    else -> Unit
+                }
+            }
+        }
+        lifecycleScope.launchWhenStarted {
+            viewModel.bestProducts.collectLatest{
+                when (it){
+                    is  Resource.Loading ->{
+                        showBestProductsLoading()
+                    }
+                    is Resource.Success ->{
+                        bestProductsAdapter.differ.submitList(it.data)
+                        hideBestProductsLoading()
+                    }
+                    is Resource.Error ->{
+                        Toast.makeText(requireContext(), it.message.toString(), Toast.LENGTH_SHORT).show()
+                        hideBestProductsLoading()
+                    }
+                    else -> Unit
+                }
+            }
+        }
+    }
 }
