a69fd17259be483938b7bccf59de7f9aab940430
Teslanot <daniilaleks8@gmail.com>
2024-05-30 17:32:51 +0300

    bug fixs( bottom navigation)

--- app/src/main/java/com/dan/marketapp/activities/ShoppingActivity.kt
+++ app/src/main/java/com/dan/marketapp/activities/ShoppingActivity.kt
@@ -30,11 +30,7 @@ class ShoppingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
 
         val navController = findNavController(R.id.nav_host_fragment)
         binding.bottomNavigation.setupWithNavController(navController)
